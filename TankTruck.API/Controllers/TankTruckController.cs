10d54c7434ec24ee6c629b1ce97c4c468d57f217
Sean Pietersen <seanpietersen7@gmail.com>
2022-03-22 12:49:40 +0200

    Feat: Adding API

---
+++ TankTruck.API/Controllers/TankTruckController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TankTruck.App;
+
+namespace TankTruck.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TankTruckController : ControllerBase
+    {
+
+        [HttpGet("tankvol/{h}/{d}/{vt}")]
+        public ActionResult GetVolumeOfTank(int h, int d, int vt)
+        {
+            IVolumeOfTank volume = new VolumeOfTank();
+
+
+            var result = volume.TankVol(h, d, vt);
+
+            return Ok(result);
+
+        }
+    }
+}
